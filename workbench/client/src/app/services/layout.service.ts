094dbd80a63fc05e1f27a5824813b1b666ddeb58
WooyeongJang <jangwy8838@gmail.com>
2024-08-12 21:36:26 +0900

    modify UI

--- workbench/client/src/app/services/layout.service.ts
+++ workbench/client/src/app/services/layout.service.ts
@@ -6,21 +6,21 @@ import { filter } from 'rxjs';
 export class LayoutService {
   readonly navigation = computed(() => this.showNavigator());
   private readonly showNavigator: WritableSignal<boolean> = signal(false);
-  private readonly router  = new Router();
+  private readonly router = new Router();
 
   constructor() {
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd),
     ).subscribe({
       next: event => {
-        if (event instanceof NavigationEnd)this.setNavigator(event.url);
+        if (event instanceof NavigationEnd) this.setNavigator(event.url);
       }
     });
   }
 
   private setNavigator(url: string) {
     const hiddenPattern = [
-      /(^\/traindb$|^\/traindb\/.*)/
+      /(^(\/$|\/traindb$|^\/traindb\/).*)/
     ];
     const hidden = hiddenPattern.some(pattern => pattern.test(url));
     this.showNavigator.set(!hidden);
